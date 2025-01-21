5ade65302883e50ff317853c6253010440ab2d89
kganya <kganyakekana@gmail.com>
2025-01-21 14:26:25 -0800

    2nd commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -198,7 +198,7 @@ export default async function Home() {
           </div>
         </header>
         <main>
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 bg-gray-700">
+          <div className="mx-auto max-w-7xl py-6  bg-gray-700">
             {
               <Dashboard/>
             }
